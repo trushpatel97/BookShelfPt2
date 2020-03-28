48bfb349c8248c7ef66fb95ad4cef03f4ed55fac
Trush <33270737+trushpatel97@users.noreply.github.com>
2020-03-28 19:00:23 -0400

    Update BookListFragment.java

--- app/src/main/java/edu/temple/bookshelf/BookListFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookListFragment.java
@@ -1,124 +1,94 @@
-package edu.temple.bookshelf;
-
+package com.example.bookshelflab;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BookListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BookListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BookListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    ArrayList<String> books;
-    public final static String BOOK_ID = "books";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public BookListFragment() {
+    ArrayList<String> books;//Creating an array list for the books
+    public final static String BOOKS_KEY = "books";//Giving it a string for the books
+    private OnBookSelectedInterface mListener;//Interface variable
+    public BookListFragment() {//empty constructor required for fragment
         // Required empty public constructor
     }
-
+    public interface OnBookSelectedInterface {//interface for communication
+        void bookSelected(int position);//we want to give it the bookSelected's position later
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param books ArrayList<String> of book titles
      * @return A new instance of fragment BookListFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static BookListFragment newInstance(ArrayList<HashMap> books) {
-        BookListFragment bookListFragment = new BookListFragment();
-        Bundle args = new Bundle();
-        args.putSerializable(BOOK_ID, books);
-        bookListFragment.setArguments(args);
-        return bookListFragment;
+    public static BookListFragment newInstance(ArrayList<String> books) {//Creating the new instance that will be used to communicate the book selected
+        BookListFragment bookListFragment = new BookListFragment();//new fragment list
+        Bundle args = new Bundle();//making bundle for communication
+        args.putStringArrayList(BOOKS_KEY, books);//puuting the book name in the key
+        bookListFragment.setArguments(args);//setting the arguement to take bundle
+        return bookListFragment;//return this specific book list fragment
     }
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {//onCreate this is what we want to load onCreate passing the data to be specific
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        Bundle args = getArguments();//getting the arguements on create
+        if (args != null) {//if we got arguements, then we want to run through all of them until we have no more
+            books = args.getStringArrayList(BOOKS_KEY);//get the string's array list specific element by key
         }
     }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ListView lv = (ListView) inflater.inflate(R.layout.fragment_book_list,container,false);
-        lv.setAdapter(new ArrayAdapter<>((Context) mListener, android.R.layout.simple_list_item_1,books));
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {//OnCreateView this is what we want to display
+        // Inflate the layout for this fragment
+        ListView listView = (ListView) inflater.inflate(R.layout.fragment_book_list, container, false);//Inflate the fragmentBookList
+        listView.setAdapter(new ArrayAdapter<>((Context) mListener, android.R.layout.simple_list_item_1, books));//set adapter to the books
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {//on item selection run code below
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position==0){
-                    mListener.bookSelected(position);
-                }else if(position==1){
-                    mListener.bookSelected(position);
-                }else if(position==2){
-                    mListener.bookSelected(position);
-                }else if(position==3){
-                    mListener.bookSelected(position);
-                }else if(position==4){
-                    mListener.bookSelected(position);
-                }else if(position==5){
-                    mListener.bookSelected(position);
-                }else if(position==6){
-                    mListener.bookSelected(position);
-                }else if(position==7){
-                    mListener.bookSelected(position);
-                }else if(position==8){
-                    mListener.bookSelected(position);
-                }else if(position==9){
-                    mListener.bookSelected(position);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {//onItemClick, we want to listen to which position was selected
+                if(position==0){//if position 0
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==1){//if position 1
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==2){//if position 2
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==3){//if position 3
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==4){//if position 4
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==5){//if position 5
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==6){//if position 6
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==7){//if position 7
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==8){//if position 8
+                    mListener.bookSelected(position);//tell listener we selected this book
+                }else if(position==9){//if position 9
+                    mListener.bookSelected(position);//tell listener we selected this book
                 }
             }
         });
-        return inflater.inflate(R.layout.fragment_book_detail, container, false);
+        return listView;//return this list view
     }
-
-
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context) {//onAttach,  called when a fragment is attached to activity
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
+        if (context instanceof OnBookSelectedInterface) {//if context is an instance of onBOokSelectedInterface
+            mListener = (OnBookSelectedInterface) context;//then we want to set listener to context
+        } else {//else
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnBookSelectedInterface");//throw this exception saying we have not implemented an interface
         }
     }
-
     @Override
-    public void onDetach() {
+    public void onDetach() {//onDetach from the activtity
         super.onDetach();
-        mListener = null;
+        mListener = null;//then we want to set listener to null
     }
-
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -129,7 +99,5 @@ public class BookListFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        void bookSelected(int position);
-    }
 }
+
