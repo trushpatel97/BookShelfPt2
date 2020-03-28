3732593c3e121b55171e4e63e22d3565a7f4275e
Trush <33270737+trushpatel97@users.noreply.github.com>
2020-03-28 19:20:12 -0400

    Update BookDetailFragment.java
    Adding the detail fragment

--- app/src/main/java/edu/temple/bookshelf/BookDetailFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookDetailFragment.java
@@ -1,124 +1,43 @@
-package edu.temple.bookshelf;
-
-import android.content.Context;
-import android.net.Uri;
+package com.example.bookshelflab;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import static edu.temple.bookshelf.BookListFragment.BOOK_ID;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BookDetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BookDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BookDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    String bookTitle;
-    public final static String BOOK_ID_KEY= "books";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public BookDetailFragment() {
-        // Required empty public constructor
+import androidx.fragment.app.Fragment;
+public class BookDetailFragment extends Fragment {//Extending fragment because its a fragment this fragment will show details of the book
+    TextView textView;//Creating textView
+    public static final String BOOK_TITLE_KEY = "book title";//Creating a default value
+    String bookTitle;//Creating booktitle variable
+    public BookDetailFragment() {//Creating a constructor as required
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BookDetailFragment newInstance(HashMap books) {
-        BookDetailFragment bookDetailFragment = new BookDetailFragment();
-        Bundle args = new Bundle();
-        args.putSerializable(BOOK_ID_KEY, books);
-        bookDetailFragment.setArguments(args);
-        return bookDetailFragment;
+    public static BookDetailFragment newInstance(String bookTitle) {//Creating a new instance that will be used depending on which book selected
+        BookDetailFragment bookDetailsFragment = new BookDetailFragment();//Creating  bookDetailsFragment that will be used for a specific book
+        Bundle args = new Bundle();//creating bundle that will be used for args
+        args.putString(BOOK_TITLE_KEY, bookTitle);//putting the bookTitle to the booktitlekey
+        bookDetailsFragment.setArguments(args);//setting the args from the bundle to communicate
+        return bookDetailsFragment;//returning the bookDetailFragment
     }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {//on create this is what data we want passed
         super.onCreate(savedInstanceState);
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            Serializable hashMap = bundle.getSerializable(BOOK_ID_KEY);
+        Bundle args = getArguments();//get the bundle args
+        if (args != null) {//until no more args to retrieve
+            bookTitle = args.getString(BOOK_TITLE_KEY);//getString of booktitle
         }
     }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView tv;
-        tv = (TextView) inflater.inflate(R.layout.fragment_book_detail,container,false);
-        if (bookTitle!=null){
-            tv.setGravity(Gravity.CENTER);
-            tv.setText(bookTitle);
-            tv.setTextSize(20);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {//onCreateView this is what we want to display
+        textView = (TextView) inflater.inflate(R.layout.fragment_book_detail, container, false);//inflate book detail fragment
+        if (bookTitle != null) {//until no more book titles
+            displayBook(bookTitle);//display this book name
         }
-        return inflater.inflate(R.layout.fragment_book_detail, container, false);
+        return textView;//return the textView
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void displayBook(String title) {//function to display book
+        textView.setGravity(Gravity.CENTER);//center the text
+        textView.setText(title);//set the text to the title
+        textView.setTextSize(20);//change font size
     }
 }
